e5cdac27029eb0a62bbc7aa343ea635ea793e384
Judy Ngai <judy.ngai1297@gmail.com>
2020-08-04 14:39:55 -0400

    first commit

---
+++ lib/rumbl_web/controllers/session_controller.ex
@@ -0,0 +1,31 @@
+defmodule RumblWeb.SessionController do
+  use RumblWeb, :controller
+
+  def new(conn, __) do
+    render(conn, "new.html")
+  end
+
+  def delete(conn, _) do
+    conn
+    |> RumblWeb.Auth.logout()
+    |> redirect(to: Routes.page_path(conn, :index))
+  end
+
+  def create(
+    conn,
+    %{"session" => %{"username" => username, "password" => pass}}
+  ) do
+    case Rumbl.Accounts.authenticate_by_username_and_pass(username, pass) do
+      {:ok, user} ->
+        conn
+        |> RumblWeb.Auth.login(user)
+        |> put_flash(:info, "Welcome back")
+        |> redirect(to: Routes.page_path(conn, :index))
+
+      {:error, _reason} ->
+        conn
+        |> put_flash(:error, "invalid username or password")
+        |> render("new.html")
+    end
+  end
+end
